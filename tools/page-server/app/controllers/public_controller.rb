31e94cb5e1d2578047d4b6b3f1c80d21e7afd6f6
Will Smart <will.r.smart@gmail.com>
2018-09-13 16:40:23 +1200

    Remove page-server

--- tools/page-server/app/controllers/public_controller.rb
+++
@@ -1,9 +0,0 @@
-class PublicController < ActionController::Base
-  #protect_from_forgery except: [:templates,:models]
-
-  layout 'base'
-
-  # there is only one page, here it is
-  def page
-  end
-end
